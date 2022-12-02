669cc63096b60ca3d5c3f9b2a28cddff158832e4
AuroraFranceschini <115407804+AuroraFranceschini@users.noreply.github.com>
2022-12-02 18:20:08 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int a = 0;
+    cin >> a;
+    if (numeroprimo (a)){
+        cout << "numero primo" << endl;
+    } else {
+        cout << "numero non primo" << endl;
+    }
+
+
+    return 0;
 }
