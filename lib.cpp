f3cd140064ace8a4b5d5cc8c0270cea6931151f9
AuroraFranceschini <115407804+AuroraFranceschini@users.noreply.github.com>
2022-12-02 18:19:34 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
-
+int numeroprimo (int a) {
+    bool b = 0;
+    int i = 2;
+    if(a != 2) {
+        for (; i<a; i++) {
+            if (a%i == 0){
+                break;
+            }
+        }
+    } else {
+        b = true;
+    }
+    if (a == i) {
+        b = true;
+    }
+    return b;
+}
